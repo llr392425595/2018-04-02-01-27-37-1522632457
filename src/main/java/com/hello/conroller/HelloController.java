c1629fe6b9a5558636ace8bad808284f22afd765
llr392425595 <392425595@qq.com>
2018-05-07 11:05:33 +0800

    .

--- src/main/java/com/hello/conroller/HelloController.java
+++ src/main/java/com/hello/conroller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> string() {
 
-        return new ResponseEntity<String>("hello update", HttpStatus.OK);
+        return new ResponseEntity<String>("hello", HttpStatus.OK);
     }
 
 }
